8ece4ccde4da4a389fc3cc812c2aa3688b203984
Daniel Friis <d@friis.me>
2025-06-12 14:09:15 +0100

    first commit

---
+++ lib/ruby_llm/schema.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "schema/version"
+
+module RubyLlm
+  module Schema
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
