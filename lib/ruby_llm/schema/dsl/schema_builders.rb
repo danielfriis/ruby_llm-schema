0565f7ee7e2dd16a869a09befeffc7f3233836ba
Daniel Friis <d@friis.me>
2025-08-26 08:46:56 +0200

    Reorganize schema_builders and utilities

--- lib/ruby_llm/schema/dsl/schema_builders.rb
+++ lib/ruby_llm/schema/dsl/schema_builders.rb
@@ -57,6 +57,7 @@ module RubyLLM
             # If the block returned a Schema class or instance, convert it to inline schema
             elsif schema_class?(result) && sub_schema.properties.empty?
               schema_class_to_inline_schema(result).merge(description ? {description: description} : {})
+            # Block didn't return reference or schema, so we build an inline object schema
             else
               {
                 type: "object",
@@ -88,6 +89,87 @@ module RubyLLM
             description: description,
             anyOf: schemas
           }.compact
+        end
+
+        private
+
+        def determine_array_items(of, &)
+          return collect_schemas_from_block(&).first if block_given?
+          return send("#{of}_schema") if primitive_type?(of)
+          return reference(of) if of.is_a?(Symbol)
+          return schema_class_to_inline_schema(of) if schema_class?(of)
+
+          raise InvalidArrayTypeError, "Invalid array type: #{of.inspect}. Must be a primitive type (:string, :number, etc.), a symbol reference, a Schema class, or a Schema instance."
+        end
+
+        def determine_object_reference(of, description = nil)
+          result = case of
+          when Symbol
+            reference(of)
+          when Class
+            if schema_class?(of)
+              schema_class_to_inline_schema(of)
+            else
+              raise InvalidObjectTypeError, "Invalid object type: #{of.inspect}. Class must inherit from RubyLLM::Schema."
+            end
+          else
+            if schema_class?(of)
+              schema_class_to_inline_schema(of)
+            else
+              raise InvalidObjectTypeError, "Invalid object type: #{of.inspect}. Must be a symbol reference, a Schema class, or a Schema instance."
+            end
+          end
+
+          description ? result.merge(description: description) : result
+        end
+
+        def collect_schemas_from_block(&block)
+          schemas = []
+          schema_builder = self
+
+          context = Object.new
+
+          # Dynamically create methods for all schema builders
+          schema_builder.methods.grep(/_schema$/).each do |schema_method|
+            type_name = schema_method.to_s.sub(/_schema$/, "")
+
+            context.define_singleton_method(type_name) do |name = nil, **options, &blk|
+              schemas << schema_builder.send(schema_method, **options, &blk)
+            end
+          end
+
+          # Allow Schema classes to be accessed in the context
+          context.define_singleton_method(:const_missing) do |name|
+            const_get(name) if const_defined?(name)
+          end
+
+          context.instance_eval(&block)
+          schemas
+        end
+
+        def schema_class_to_inline_schema(schema_class_or_instance)
+          # Handle both Schema classes and Schema instances
+          schema_class = if schema_class_or_instance.is_a?(Class)
+            schema_class_or_instance
+          else
+            schema_class_or_instance.class
+          end
+
+          # Directly convert schema class to inline object schema
+          {
+            type: "object",
+            properties: schema_class.properties,
+            required: schema_class.required_properties,
+            additionalProperties: schema_class.additional_properties
+          }.tap do |schema|
+            # For instances, prefer instance description over class description
+            description = if schema_class_or_instance.is_a?(Class)
+              schema_class.description
+            else
+              schema_class_or_instance.instance_variable_get(:@description) || schema_class.description
+            end
+            schema[:description] = description if description
+          end
         end
       end
     end
